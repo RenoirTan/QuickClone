d4b40828d325f1db6ed68aee95f00410e59102df
RenoirTan <renoirtan2005@gmail.com>
2022-03-01 00:13:44 +0800

    0.4: add features

--- quickclone/config/common.py
+++ quickclone/config/common.py
@@ -11,3 +11,14 @@ USER_CONFIG_FILE: Path = Path.home() / ".config" / "quickclone.toml"
 """
 The path to the user's configuration file.
 """
+
+
+USER_CACHE_FOLDER: Path = Path.home() / ".cache" / "quicktoml"
+"""
+The path to the cache directory owned by the user to store QuickClone data.
+"""
+
+USER_HISTORY_CACHE_FILE: Path = USER_CACHE_FOLDER / "history.toml"
+"""
+The path to the cache file storing the user's usage history of QuickClone.
+"""
